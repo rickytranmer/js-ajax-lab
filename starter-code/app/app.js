4e5696b0d4bdc889db6745836950c4a49bd31f58
rickytranmer <rwtranmer@gmail.com>
2017-12-18 15:52:56 -0700

    ready document, comment developmental steps

---
+++ starter-code/app/app.js
@@ -0,0 +1,9 @@
+$(function() {
+    console.log( "DOM is ready" );
+    //TODO - Create a list of all books as list items
+    //TODO - Each book's list item of title, author, release, img
+
+    //TODO - Put all form info in variable newBook
+    //TODO - Post newBook to den-super-crud
+    //TODO - Remove list of books, create new one
+});
