15db1c0cc9747ba83b6937b3425b6a14e670e54e
rickytranmer <rwtranmer@gmail.com>
2017-12-18 20:40:46 -0700

    update jquery link, print list items on screen

--- starter-code/app/app.js
+++ starter-code/app/app.js
@@ -1,7 +1,19 @@
 $(function() {
     console.log( "DOM is ready" );
     //TODO - Create a list of all books as list items
-    //TODO - Each book's list item of title, author, release, img
+    var booksObject = {};
+    var nextBook;
+    var ajax = $.get('https://den-super-crud.herokuapp.com/books/')//;
+    .done(function(data) {
+        	console.log(data.books);
+        	for (let i=0; i<data.books.length; i++) {
+        		// - Makes li ul for each book, with title, author, releaseDate, & image as li
+        		booksObject = JSON.parse(JSON.stringify(data.books[i]));
+        		console.log(booksObject.author);
+        		console.log(data.books[i]);
+        		$('#books').append($('<li><ul><li>' + booksObject.author + '</li><li>' + booksObject.title + '</li><li>' + booksObject.releaseDate + '</li><li><img src="' + booksObject.image + '"></li></ul></li>'));
+        	}
+    });
 
     //TODO - Put all form info in variable newBook
     //TODO - Post newBook to den-super-crud
