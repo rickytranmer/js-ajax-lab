9fda5124b3494b3a588358776d4683a4362f0619
rickytranmer <rwtranmer@gmail.com>
2017-12-18 22:41:16 -0700

    take in form input, update book listings

--- starter-code/app/app.js
+++ starter-code/app/app.js
@@ -1,21 +1,37 @@
 $(function() {
     console.log( "DOM is ready" );
-    //TODO - Create a list of all books as list items
-    var booksObject = {};
-    var ajax = $.get('https://den-super-crud.herokuapp.com/books/')//;
-    .done(function(data) {
-        	console.log(data.books);
-        	for (let i=0; i<data.books.length; i++) {
-        		// - Makes li ul for each book, with title, author, releaseDate, & image as li
-        		booksObject = JSON.parse(JSON.stringify(data.books[i]));
-        		console.log(booksObject.author);
-        		console.log(data.books[i]);
-        		$('#books').append($('<li><ul id="' + booksObject._id + '"><li>' + booksObject.title + '</li><li>' + booksObject.author + '</li><li>' + booksObject.releaseDate + '</li><li><img src="' + booksObject.image + '"></li></ul></li>'));
-        	}
-    });
+    // - Create a list of all books as list items
+    function showBooks() {
+    	var ajax = $.get('https://den-super-crud.herokuapp.com/books/')//;
+    	  .done(function(data) {
+    	    console.log(data);
+    	  	for (let i = 0; i < data.books.length; i++) {
+    	   		// - Makes li ul for each book, with title, author, releaseDate, & image as li
+    	   		$('#books').append($('<li><ul id="' + data.books[i]._id + '"><li>' + data.books[i].title + '</li><li>' + data.books[i].author + '</li><li>' + data.books[i].releaseDate + '</li><li><img src="' + data.books[i].image + '"></li></ul></li>'));
+    	   	}
+    	  });
+    }
 
-    //TODO - Put all form info in variable newBook
-    //TODO - Post newBook to den-super-crud
-    //TODO - Remove list of books, create new one
-    // $('#books').empty();
+    showBooks();
+    
+    // - Put all form info in variable newBook
+    $("form").on("submit", function(event){
+    	let newBook = {
+    		title: $('#book-title').val(),
+    		author: $('#book-author').val(),
+    		image: $('#book-image').val(),
+    		releaseDate: $('#book-release-date').val()
+    	};
+    	console.log(newBook);
+
+    	// - Post newBook to den-super-crud
+	    $.post('https://den-super-crud.herokuapp.com/books', newBook)
+	      .done(function(data) {
+	      console.log(data);
+		});
+	      
+	    // - Remove list of books, create new one
+	    $('#books').empty();
+	    showBooks();
+    });
 });
