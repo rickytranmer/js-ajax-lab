a8d3f067ff916f7a337222c9560491c0ec4c02aa
rickytranmer <rwtranmer@gmail.com>
2017-12-18 21:38:22 -0700

    all input fields required

--- starter-code/app/app.js
+++ starter-code/app/app.js
@@ -2,7 +2,6 @@ $(function() {
     console.log( "DOM is ready" );
     //TODO - Create a list of all books as list items
     var booksObject = {};
-    var nextBook;
     var ajax = $.get('https://den-super-crud.herokuapp.com/books/')//;
     .done(function(data) {
         	console.log(data.books);
@@ -11,11 +10,12 @@ $(function() {
         		booksObject = JSON.parse(JSON.stringify(data.books[i]));
         		console.log(booksObject.author);
         		console.log(data.books[i]);
-        		$('#books').append($('<li><ul><li>' + booksObject.author + '</li><li>' + booksObject.title + '</li><li>' + booksObject.releaseDate + '</li><li><img src="' + booksObject.image + '"></li></ul></li>'));
+        		$('#books').append($('<li><ul id="' + booksObject._id + '"><li>' + booksObject.title + '</li><li>' + booksObject.author + '</li><li>' + booksObject.releaseDate + '</li><li><img src="' + booksObject.image + '"></li></ul></li>'));
         	}
     });
 
     //TODO - Put all form info in variable newBook
     //TODO - Post newBook to den-super-crud
     //TODO - Remove list of books, create new one
+    // $('#books').empty();
 });
